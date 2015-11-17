db28fe8cc594b6cb6cfd7c8e4b29be43d09d5e1a
Alexey Privezentsev <remake@iao.ru>
2015-11-17 17:00:31 +0600

    Fix wsgi.py for the wadis node

--- nodes/wadis/wsgi.py
+++ nodes/wadis/wsgi.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
+
 # EDIT THE FOLLOWING TWO LINES
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'nodes.' + os.path.basename(os.path.dirname(__file__)) + '.settings'
 
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
